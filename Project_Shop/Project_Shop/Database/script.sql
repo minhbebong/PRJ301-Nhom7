4c2a2bd9e91050c0176d787746263480fde4f268
minh nguyen <hichic375@gmail.com>
2022-06-14 22:23:05 +0700

    database MyShop

---
+++ Project_Shop/Project_Shop/Database/script.sql
@@ -0,0 +1,205 @@
+﻿Create database MyShop1
+
+GO
+/****** Object:  Table [dbo].[bill] bảng hóa đơn    ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[bill](
+	[bill_id] [bigint] IDENTITY(1,1) NOT NULL,
+	[user_id] [bigint] NOT NULL,
+	[total] [money] NOT NULL,
+	[payment] [varchar](250) NOT NULL,
+	[address] [ntext] NOT NULL,
+	[date] [date] NOT NULL,
+	[phone] [bigint] NOT NULL,
+ CONSTRAINT [PK_bill] PRIMARY KEY CLUSTERED 
+(
+	[bill_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[bill_detail]   Bảng Chi tiết hóa đơn ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[bill_detail](
+	[detail_id] [bigint] IDENTITY(1,1) NOT NULL,
+	[bill_id] [bigint] NOT NULL,
+	[product_id] [varchar](100) NOT NULL,
+	[quantity] [int] NOT NULL,
+	[size] [nchar](10) NOT NULL,
+	[color] [nvarchar](150) NOT NULL,
+	[price] [money] NOT NULL,
+ CONSTRAINT [PK_bill_detail] PRIMARY KEY CLUSTERED 
+(
+	[detail_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[cart]    Bảng giỏ hàng order ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[cart](
+	[cart_id] [bigint] NOT NULL,
+	[product_id] [varchar](100) NOT NULL,
+	[product_name] [varchar](255) NOT NULL,
+	[product_img] [varchar](255) NOT NULL,
+	[product_price] [float] NOT NULL,
+	[total] [float] NOT NULL,
+	[quantity] [int] NOT NULL,
+ CONSTRAINT [PK_cart] PRIMARY KEY CLUSTERED 
+(
+	[cart_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[category]   Bảng nguồn loại sản phẩm******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[category](
+	[category_id] [int] IDENTITY(1,1) NOT NULL,
+	[category_name] [varchar](255) NOT NULL,
+ CONSTRAINT [PK_category] PRIMARY KEY CLUSTERED 
+(
+	[category_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[product]    Bảng sản phẩm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[product](
+	[product_id] [varchar](100) NOT NULL,
+	[category_id] [int] NOT NULL,
+	[product_name] [nvarchar](max) NOT NULL,
+	[product_price] [money] NOT NULL,
+	[product_describe] [nvarchar](max) NOT NULL,
+	[quantity] [int] NOT NULL,
+	[img] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_product] PRIMARY KEY CLUSTERED 
+(
+	[product_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[product_color]    Bảng lựa chọn màu sản phẩm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[product_color](
+	[product_id] [varchar](100) NOT NULL,
+	[color] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[product_size]    Bảng chọn size sản phẩm******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[product_size](
+	[product_id] [varchar](100) NOT NULL,
+	[size] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[users]    Bảng người dùng ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[users](
+	[user_id] [bigint] IDENTITY(1,1) NOT NULL,
+	[user_name] [nvarchar](200) NULL,
+	[user_email] [varchar](255) NOT NULL,
+	[user_pass] [nvarchar](255) NOT NULL,
+	[isAdmin] [nvarchar](50) NULL,
+ CONSTRAINT [PK_users] PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET IDENTITY_INSERT [dbo].[bill] ON 
+
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (45, 5, 648000.0000, N'MOMO', N'Hà Đông, Hà Nội', CAST(N'2021-11-04' AS Date), 919208356)
+
+SET IDENTITY_INSERT [dbo].[bill] OFF
+GO
+SET IDENTITY_INSERT [dbo].[bill_detail] ON 
+
+INSERT [dbo].[bill_detail] ([detail_id], [bill_id], [product_id], [quantity], [size], [color], [price]) VALUES (40, 45, N'T2129', 1, N'XS        ', N'White', 270000.0000)
+
+SET IDENTITY_INSERT [dbo].[bill_detail] OFF
+GO
+SET IDENTITY_INSERT [dbo].[category] ON 
+
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (1, N'SHIRT')
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (2, N'T-SHIRT')
+
+SET IDENTITY_INSERT [dbo].[category] OFF
+GO
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'AT533', 2, N'Champion Graphic Big Logo T-Shirt', 149000.0000, N'Cổ Tròn
+Vạt Ngang
+Tay Ngắn
+Hình In Trướ', 186, N'img/logo2.png')
+
+GO
+INSERT [dbo].[product_color] ([product_id], [color]) VALUES (N'T2129', N'Grey')
+INSERT [dbo].[product_color] ([product_id], [color]) VALUES (N'T2129', N'Blue')
+
+
+GO
+INSERT [dbo].[product_size] ([product_id], [size]) VALUES (N'T2129', N'L')
+INSERT [dbo].[product_size] ([product_id], [size]) VALUES (N'T2129', N'XL')
+
+GO
+SET IDENTITY_INSERT [dbo].[users] ON 
+
+INSERT [dbo].[users] ([user_id], [user_name], [user_email], [user_pass], [isAdmin]) VALUES (1, N'Admin', N'minhbebong@gmail.com', N'minh1423', N'TRUE')
+INSERT [dbo].[users] ([user_id], [user_name], [user_email], [user_pass], [isAdmin]) VALUES (2, N'Cuong', N'cuonghung', N'123456', N'FALSE')
+GO
+ALTER TABLE [dbo].[bill]  WITH CHECK ADD  CONSTRAINT [FK_bill_users] FOREIGN KEY([user_id])
+REFERENCES [dbo].[users] ([user_id])
+GO
+ALTER TABLE [dbo].[bill] CHECK CONSTRAINT [FK_bill_users]
+GO
+ALTER TABLE [dbo].[bill_detail]  WITH CHECK ADD  CONSTRAINT [FK_bill_detail_bill] FOREIGN KEY([bill_id])
+REFERENCES [dbo].[bill] ([bill_id])
+GO
+ALTER TABLE [dbo].[bill_detail] CHECK CONSTRAINT [FK_bill_detail_bill]
+GO
+ALTER TABLE [dbo].[bill_detail]  WITH CHECK ADD  CONSTRAINT [FK_bill_detail_product] FOREIGN KEY([product_id])
+REFERENCES [dbo].[product] ([product_id])
+GO
+ALTER TABLE [dbo].[bill_detail] CHECK CONSTRAINT [FK_bill_detail_product]
+GO
+ALTER TABLE [dbo].[cart]  WITH CHECK ADD  CONSTRAINT [FK_cart_product] FOREIGN KEY([product_id])
+REFERENCES [dbo].[product] ([product_id])
+GO
+ALTER TABLE [dbo].[cart] CHECK CONSTRAINT [FK_cart_product]
+GO
+ALTER TABLE [dbo].[product]  WITH CHECK ADD  CONSTRAINT [FK_product_category] FOREIGN KEY([category_id])
+REFERENCES [dbo].[category] ([category_id])
+GO
+ALTER TABLE [dbo].[product] CHECK CONSTRAINT [FK_product_category]
+GO
+ALTER TABLE [dbo].[product_color]  WITH CHECK ADD  CONSTRAINT [FK_product_color_product] FOREIGN KEY([product_id])
+REFERENCES [dbo].[product] ([product_id])
+GO
+ALTER TABLE [dbo].[product_color] CHECK CONSTRAINT [FK_product_color_product]
+GO
+ALTER TABLE [dbo].[product_size]  WITH CHECK ADD  CONSTRAINT [FK_product_size_product] FOREIGN KEY([product_id])
+REFERENCES [dbo].[product] ([product_id])
+GO
+ALTER TABLE [dbo].[product_size] CHECK CONSTRAINT [FK_product_size_product]
+GO
